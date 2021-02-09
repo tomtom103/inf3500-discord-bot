cb8078cb3a11f96017c83fb3ecad21ebc56b8760
Thomas Caron <thomas.caron00@gmail.com>
2021-02-09 15:27:10 -0500

    Changed bot syntax

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-require('dotenv').config();
-const Discord = require('discord.js');
-const bot = new Discord.Client();
+const { Client, MessageEmbed } = require('discord.js');
 
-// Bot secret key, must be contained in a .env file in the same directory
-const TOKEN = process.env.TOKEN;
+require('dotenv').config();
+
+const bot = new Client();
+
+const TOKEN = process.env.TOKEN
 
 bot.login(TOKEN);
 
-// Roles that can use the !poll and !syntax commands
 const roles = ["répétiteurs", "chargés-labos", "prof"]
 
-// The string is used for the text part, and the emoji is used for the raection.
-// You can copy and paste different emojis from https://getemoji.com/
-const emojis = {
-  circles: [
+const allowed_users = [
+    { username: 'thom', discriminator: '3270'},
+    { username: 'Pierre Langlois', discriminator: '4976'},
+];
+
+const emojis = [
     { name: 'red_circle', value: '🔴'},
     { name: 'green_circle', value: '🟢'},
     { name: 'yellow_circle', value: '🟡'},
@@ -26,65 +28,80 @@ const emojis = {
     { name: 'blue_square', value: '🟦'},
     { name: 'orange_square', value: '🟧'},
     { name: 'purple_square', value: '🟪'}
-  ]
+]
+
+function verifyCommandPermission(msg) {
+    return (msg.member.roles.cache.some(role => roles.includes(role.name)) || allowed_users.find(user => {
+        return user.username === msg.author.username && user.discriminator === msg.author.discriminator;
+    }));
 }
 
 bot.on('ready', () => {
-  console.info(`Logged in as ${bot.user.tag}!`);
+    console.info(`Logged in as ${bot.user.tag}`);
 });
 
 bot.on('message', msg => {
-  if(msg.content.startsWith('!syntax') && msg.member.roles !== null && msg.member.roles.some(role => roles.includes(role.name))) {
-    msg.delete();
-    msg.channel.send(`Syntax: \n !poll "Ici va le titre" [Première option | Deuxième Option]`)
-  }
-});
-
-bot.on('message', msg => {
-  if(msg.content) {
-    // Array with: anything in brackets, anything in quotes, anything separated by spaces (in that hierarchy)
-    var args = msg.content.trim().match(/(?:[^\s"\[]+|\[[^\[]*\]|"[^"]*")+/g);
-    if(args[0].toLowerCase() == '!poll') {
-      if(msg.member.roles != null) {
-        if(msg.member.roles.some(role => roles.includes(role.name))) {
-          args.shift();
-          msg.delete();
-          if(
-            args.length > 1 &&
-            args[0].charAt(0) === '"' &&
-            args[0].charAt(args[0].length - 1) === '"' &&
-            args[1].charAt(0) === '[' &&
-            args[1].charAt(args[1].length - 1) === ']' 
-          ) {
-            var title = args.shift().slice(1, -1);
-            var choices = args.shift().slice(1,-1).split('|').map(Function.prototype.call, String.prototype.trim);
-    
-            const embed = new Discord.RichEmbed()
-            .setTitle(title)
-            .setColor(0x00AE86);
-    
-            var embedString = "";
-            
-            emojis.circles.slice(0, choices.length).forEach((item, index) => {
-              embedString += ':' + item.name + ':' + ' : ' + choices[index] + '\n\n';
-            })
-    
-            embed.addField("Choix", "\n" + embedString)
-    
-            msg.channel.send(embed)
-            .then((message) => {
-              emojis.circles.slice(0, choices.length).forEach(item => {
-                message.react(item.value)
-              })
-            })
-            .catch((err) => {
-              console.log(err);
-            })
-          } else {
-            msg.channel.send("Hmmm... you seem to be missing some arguments. Try !syntax to view the syntax")
-          }
-        } 
-      } 
+    if(msg.content.startsWith('!ping')) {
+        if(verifyCommandPermission(msg)) {
+            msg.delete();
+            msg.channel.send("PONG");
+        }
     }
-  }
+})
+
+bot.on('message', msg => {
+    if(msg.content.startsWith('!syntax')) {
+        if(verifyCommandPermission(msg)) {
+            msg.channel.send("Syntax: \n\n!poll Voici le titre\nVoici la première option\nDeuxieme Option\nTroisieme Option");
+        }
+    }
+})
+
+bot.on('message', (message) => {
+    if(message.content.startsWith('!poll')) {
+        if(verifyCommandPermission(message)) {
+            let args = message.content.split('\n');
+            args[0] = args[0].replace('!poll', '').trim();
+            if(args.length > 1) {
+                let title = args.shift();
+                message.delete();
+                const embed = new MessageEmbed()
+                .setTitle(title)
+                .setColor(0x00AE86);
+
+                var embedString = "";
+
+                emojis.slice(0, args.length).forEach((item, index) => {
+                    embedString += ':' + item.name + ':' + ' : ' + args[index] + '\n\n';
+                });
+
+                embed.addField("Choix", "\n\n" + embedString);
+
+                message.channel.send(embed)
+                .then((msg) => {
+                    emojis.slice(0, args.length).forEach(item => {
+                        msg.react(item.value);
+                    })
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+
+            } else if(args.length > emojis.length + 1) {
+                message.channel.send('Too many choices! Currently supported max is: ' + emojis.length);
+            }
+            
+            else  {
+                message.channel.send('Too few arguments to call function (need at least title and 1 choice');
+            }
+        }
+    }
+})
+
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('ok');
 });
+
+server.listen(3000);
